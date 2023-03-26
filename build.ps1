cc26275fa4061dcc5e2cda6f0e65a4d8a0ea6342
OwenFeik <owenhfeik@gmail.com>
2023-03-26 19:36:38 +1100

    improve build script

--- build.ps1
+++ build.ps1
@@ -1,18 +1,24 @@
-$PLUGIN_NAME = "keypirinha-firefoxtabs"
+$PLUGIN_DIRECTORY = "keypirinha-firefoxtabs"
+$EXTENSION_NAME = "keypirinha-firefoxtabs.zip"
+$PLUGIN_FILE = "FirefoxTabs.keypirinha-package"
 $BUILD_DIRECTORY = "$PSScriptRoot/build"
-$EXTENSION_FILE = "$BUILD_DIRECTORY/$PLUGIN_NAME.zip"
+$EXTENSION_FILE = "$BUILD_DIRECTORY/$EXTENSION_NAME"
 $PLUGIN_ZIP = "$BUILD_DIRECTORY/FirefoxTabs.zip"
-$PLUGIN_FILE = "$BUILD_DIRECTORY/FirefoxTabs.keypirinha-package"
+$PLUGIN_PATH = "$BUILD_DIRECTORY/$PLUGIN_FILE"
 
-New-Item -ItemType Directory -Force -Path build
+New-Item -ItemType Directory -Force -Path build | Out-Null
 Remove-Item -Path "$BUILD_DIRECTORY/*"
 
+# Build Firefox extension zip.
 Compress-Archive -Path "./extension/*" -DestinationPath "$EXTENSION_FILE"
+Write-Output "Firefox extension built at build/$EXTENSION_FILE."
+
 Compress-Archive `
     -Path `
-        "./$PLUGIN_NAME/src/firefoxtabs*",
-        "./$PLUGIN_NAME/src/lib",
-        "./$PLUGIN_NAME/LICENSE",
-        "./$PLUGIN_NAME/README.md" `
+        "./$PLUGIN_DIRECTORY/src/firefoxtabs*",
+        "./$PLUGIN_DIRECTORY/src/lib",
+        "./$PLUGIN_DIRECTORY/LICENSE",
+        "./$PLUGIN_DIRECTORY/README.md" `
     -DestinationPath "$PLUGIN_ZIP"
-Move-Item -Path "$PLUGIN_ZIP" -Destination "$PLUGIN_FILE"
+Move-Item -Path "$PLUGIN_ZIP" -Destination "$PLUGIN_PATH"
+Write-Output "Keypirinha plugin built at build/$PLUGIN_FILE."
