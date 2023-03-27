26dbdfe93c9f0331006b1da742123e5dfae9b284
OwenFeik <owenhfeik@gmail.com>
2023-03-27 12:39:17 +1100

    reorganise repo

--- build.ps1
+++ build.ps1
@@ -1,24 +1,28 @@
-$PLUGIN_DIRECTORY = "keypirinha-firefoxtabs"
-$EXTENSION_NAME = "keypirinha-firefoxtabs.zip"
-$PLUGIN_FILE = "FirefoxTabs.keypirinha-package"
-$BUILD_DIRECTORY = "$PSScriptRoot/build"
-$EXTENSION_FILE = "$BUILD_DIRECTORY/$EXTENSION_NAME"
-$PLUGIN_ZIP = "$BUILD_DIRECTORY/FirefoxTabs.zip"
-$PLUGIN_PATH = "$BUILD_DIRECTORY/$PLUGIN_FILE"
+$BUILD_DIRECTORY = "$PSScriptRoot/build"             # Build output directory
 
+$EXTENSION_NAME = "keypirinha-tabswitcher"           # Extension title
+$EXTENSION_FILE = "$EXTENSION_NAME.zip"              # Extension file name
+$EXTENSION_PATH = "$BUILD_DIRECTORY/$EXTENSION_FILE" # Extension file location
+
+$PLUGIN_FILE = "FirefoxTabs.keypirinha-package"      # kp plugin file name
+$PLUGIN_ZIP = "$BUILD_DIRECTORY/FirefoxTabs.zip"     # Intermediate file
+$PLUGIN_PATH = "$BUILD_DIRECTORY/$PLUGIN_FILE"       # kp plugin file location
+
+# Create build directory and delete any existing output files.
 New-Item -ItemType Directory -Force -Path build | Out-Null
 Remove-Item -Path "$BUILD_DIRECTORY/*"
 
 # Build Firefox extension zip.
-Compress-Archive -Path "./extension/*" -DestinationPath "$EXTENSION_FILE"
+Compress-Archive -Path "./extension/*" -DestinationPath "$EXTENSION_PATH"
 Write-Output "Firefox extension built at build/$EXTENSION_FILE."
 
+# Build kp plugin and rename to appropriate extension.
 Compress-Archive `
     -Path `
-        "./$PLUGIN_DIRECTORY/src/firefoxtabs*",
-        "./$PLUGIN_DIRECTORY/src/lib",
-        "./$PLUGIN_DIRECTORY/LICENSE",
-        "./$PLUGIN_DIRECTORY/README.md" `
+        "./src/firefoxtabs*",
+        "./src/lib",
+        "./LICENSE",
+        "./README.md" `
     -DestinationPath "$PLUGIN_ZIP"
 Move-Item -Path "$PLUGIN_ZIP" -Destination "$PLUGIN_PATH"
 Write-Output "Keypirinha plugin built at build/$PLUGIN_FILE."
